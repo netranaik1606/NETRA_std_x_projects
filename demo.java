f63d2faffd50e6b0abc18b9333d1d8f0e116c866
netranaik1606 <netranaik1606@gmail.com>
2026-03-28 11:00:32 +0530

    Add files via upload

---
+++ demo.java
@@ -0,0 +1,17 @@
+package Test1;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("name= netra naik");
+		System.out.println("std= 10th");
+		System.out.println("roll no.= 04");
+		System.out.println("school= st.francis school");
+		
+		
+	}
+	
+
+}
